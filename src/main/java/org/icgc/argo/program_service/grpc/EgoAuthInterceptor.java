84e5efbf717b19774bfb3262b63d834ad90ef3ad
Dušan Andrić <Dusan.Andric@gmail.com>
2019-04-05 13:27:13 -0400

    database support with flyway, spring data. Removes supression of exception in interceptor

--- src/main/java/org/icgc/argo/program_service/grpc/EgoAuthInterceptor.java
+++ src/main/java/org/icgc/argo/program_service/grpc/EgoAuthInterceptor.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 import io.grpc.*;
 import io.grpc.stub.StreamObserver;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -62,6 +63,7 @@ public class EgoAuthInterceptor implements ServerInterceptor {
     @Slf4j
     class EgoAuthAspect {
 
+      @SneakyThrows
       @Around("@annotation(egoAuth)")
       public Object checkIdentity(ProceedingJoinPoint pjp, EgoAuth egoAuth) {
         val egoToken = EGO_TOKEN.get();
@@ -83,7 +85,7 @@ public class EgoAuthInterceptor implements ServerInterceptor {
             return pjp.proceed();
           } catch(Throwable e) {
             log.info(e.getMessage());
-            return null;
+            throw e;
           }
         }
       }
