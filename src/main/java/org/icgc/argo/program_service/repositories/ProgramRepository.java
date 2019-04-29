6f66238090dbdfeed3a659d338f7a0ef9102c17c
Xu Deng <d8660091@gmail.com>
2019-04-29 14:57:13 -0400

    Update program repository

--- src/main/java/org/icgc/argo/program_service/repositories/ProgramRepository.java
+++ src/main/java/org/icgc/argo/program_service/repositories/ProgramRepository.java
@@ -1,12 +1,12 @@
 package org.icgc.argo.program_service.repositories;
 
-import org.icgc.argo.program_service.model.entity.Program;
+import org.icgc.argo.program_service.model.entity.ProgramEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 
-public interface ProgramRepository extends JpaRepository<Program, UUID> {
+public interface ProgramRepository extends JpaRepository<ProgramEntity, UUID> {
 
 
 }
