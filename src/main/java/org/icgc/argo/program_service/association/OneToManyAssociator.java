5c0d03822d2d4dc682e6b35efc623566b6fccf5e
Robert Tisma <rtisma@gmail.com>
2019-05-17 10:03:35 -0400

    added conflict and not_found checking

--- src/main/java/org/icgc/argo/program_service/association/OneToManyAssociator.java
+++ src/main/java/org/icgc/argo/program_service/association/OneToManyAssociator.java
@@ -28,6 +28,14 @@ import java.util.Collection;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+import static org.icgc.argo.program_service.association.AssociatorException.checkConflict;
+import static org.icgc.argo.program_service.association.AssociatorException.checkNotFound;
+import static org.icgc.argo.program_service.utils.CollectionUtils.convertToIds;
+import static org.icgc.argo.program_service.utils.CollectionUtils.difference;
+import static org.icgc.argo.program_service.utils.Joiners.COMMA_SPACE;
+
 /**
  * This class manages OneToMany BiDirectional relationships between a parent and its child entities.
  *
@@ -54,7 +62,12 @@ public class OneToManyAssociator<
   //TODO: [rtisma] check that the parent doesnt already have the child
   @Override
   public P associate(P parent, C child){
-    getChildrenFromParent(parent).add(child);
+    // check parent does not contain child
+    val childrenFromParent = getChildrenFromParent(parent);
+    val parentHasChildAlready = childrenFromParent.stream().anyMatch(x -> x.getId().equals(child.getId()));
+    checkConflict(!parentHasChildAlready, "The %s with id '%s' already contains %s with id '%s'",
+        parent.getClass().getSimpleName(), parent.getId().toString(),
+        child.getClass().getSimpleName(), child.getId().toString());
     setParentFieldForChild(parent, child);
     return parent;
   }
@@ -63,6 +76,8 @@ public class OneToManyAssociator<
   @Override
   public P disassociate(P parent, Collection<CID> childIdsToDisassociate){
     val children = getChildrenFromParent(parent);
+    val existingChildIds = convertToIds(children);
+    checkMissingChildIds(parent, childIdsToDisassociate, existingChildIds);
     disassociateSelectedChildren(children, childIdsToDisassociate);
     return parent;
   }
@@ -70,17 +85,15 @@ public class OneToManyAssociator<
   //TODO: [rtisma] check that all the parents contains all the child ids
   @Override
   public Collection<P> disassociate(Collection<P> parents, Collection<CID> childIdsToDisassociate){
+    val parentToChildIdsMap = parents.stream()
+        .collect(toMap(identity(), x -> convertToIds(getChildrenFromParent(x))));
+    parentToChildIdsMap.forEach((parent, existingChildIds) ->
+      checkMissingChildIds(parent, childIdsToDisassociate, existingChildIds)
+    );
     parents.stream()
         .map(this::getChildrenFromParent)
         .forEach(children ->  disassociateSelectedChildren(children, childIdsToDisassociate));
     return parents;
-  }
-
-  private void disassociateSelectedChildren(Collection<C> children, Collection<CID> selectedChildIds ){
-    children.stream()
-        .filter(x -> selectedChildIds.contains(x.getId()))
-        .forEach(x -> setParentFieldForChild( null, x) );
-    children.removeIf(x -> selectedChildIds.contains(x.getId()));
   }
 
   /**
@@ -96,5 +109,24 @@ public class OneToManyAssociator<
   protected void setParentFieldForChild(P parent, @NonNull C child) {
     setParentFieldForChildFunction.accept(parent, child);
   }
+
+  private void disassociateSelectedChildren(Collection<C> children, Collection<CID> selectedChildIds ){
+    children.stream()
+        .filter(x -> selectedChildIds.contains(x.getId()))
+        .forEach(x -> setParentFieldForChild( null, x) );
+    children.removeIf(x -> selectedChildIds.contains(x.getId()));
+  }
+
+  // Although stateless, left as method to reuse type boundaries. Could be static
+  private void checkMissingChildIds(P parent, Collection<CID> childIdsToDisassociate, Collection<CID> existingChildIds){
+    val nonExistingChildIds = difference(childIdsToDisassociate, existingChildIds);
+    checkNotFound(
+        nonExistingChildIds.isEmpty(),
+        "The following child ids were not found for the %s with id '%s': %s",
+        parent.getClass().getSimpleName(),
+        parent.getId().toString(),
+        "["+COMMA_SPACE.join(nonExistingChildIds)+"]");
+  }
+
 
 }
