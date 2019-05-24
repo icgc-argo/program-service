037f5ecdca819bb847dcc3540e14ed486edfee52
Alexis Li <alexis.li@oicr.on.ca>
2019-05-24 13:27:56 -0400

    Updated email wording.

--- src/main/java/org/icgc/argo/program_service/services/MailService.java
+++ src/main/java/org/icgc/argo/program_service/services/MailService.java
@@ -48,6 +48,7 @@ public class MailService {
       ctx.put("firstName", invitation.getFirstName());
       ctx.put("lastName", invitation.getLastName());
       ctx.put("inviteId", invitation.getId());
+      ctx.put("programShortName", invitation.getProgram().getShortName());
       // TODO: add join program link
       ctx.put("joinProgramLink", "#");
       template.merge(ctx, sw);
