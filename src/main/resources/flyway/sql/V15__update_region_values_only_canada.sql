8812368f0d751bca69b2dc2a4f42485a45506b5d
Jon Eubank <joneubank@gmail.com>
2020-02-24 10:51:58 -0500

    🐛 Fix Migration to allow programs with multiple regions (#300)

--- src/main/resources/flyway/sql/V15__update_region_values_only_canada.sql
+++ src/main/resources/flyway/sql/V15__update_region_values_only_canada.sql
@@ -1,7 +1,20 @@
+DELETE FROM program_region;
+DELETE FROM region;
+
 INSERT INTO region (id, name)
 VALUES
     (uuid_generate_v4(), 'Canada');
 
-UPDATE program_region SET region_id = (SELECT id FROM region WHERE name = 'Canada');
+INSERT INTO program_region (program_id, region_id)
+WITH
+    prog AS (
+        SELECT id FROM program
+    ),
+    reg AS (
+        SELECT id
+        FROM region
+        WHERE name = 'Canada'
+    )
+SELECT prog.id, reg.id
+FROM prog, reg;
 
-DELETE FROM region WHERE NOT name = 'Canada';
