e51f1ce1a2e5c9d8b299e661880f42fb07cce7ee
blabadi <bashar.allabadi@oicr.on.ca>
2021-04-21 11:39:58 -0400

    remove user creation on invite (#357)
     remove user creation on invite

--- src/main/java/org/icgc/argo/program_service/services/ProgramServiceFacade.java
+++ src/main/java/org/icgc/argo/program_service/services/ProgramServiceFacade.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 The Ontario Institute for Cancer Research. All rights reserved
+ * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
  *
  * This program and the accompanying materials are made available under the terms of the GNU Affero General Public License v3.0.
  * You should have received a copy of the GNU Affero General Public License along with
@@ -161,8 +161,6 @@ public class ProgramServiceFacade {
     val email = commonConverter.unboxStringValue(request.getEmail());
     val firstName = commonConverter.unboxStringValue(request.getFirstName());
     val lastName = commonConverter.unboxStringValue(request.getLastName());
-
-    egoService.getOrCreateUser(email, firstName, lastName);
 
     val inviteId =
         invitationService.inviteUser(
