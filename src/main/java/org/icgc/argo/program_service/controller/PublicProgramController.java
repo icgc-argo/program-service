06441a8fec924d270ec28ec95aafce4eaa8fad4b
Azher2Ali <121898125+Azher2Ali@users.noreply.github.com>
2023-03-09 13:54:00 -0500

    #379 - Updated error handling logic for program names NOT FOUND
    #379 - Updated error handling logic for non existing program names

--- src/main/java/org/icgc/argo/program_service/controller/PublicProgramController.java
+++ src/main/java/org/icgc/argo/program_service/controller/PublicProgramController.java
@@ -6,6 +6,7 @@ import org.icgc.argo.program_service.model.dto.builder.ProgramDTOBuilder;
 import org.icgc.argo.program_service.model.entity.ProgramEntity;
 import org.icgc.argo.program_service.services.ProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,14 +22,21 @@ public class PublicProgramController {
   @Autowired ProgramDTOBuilder programDTOBuilder;
 
   @GetMapping(value = "/program")
-  public ResponseEntity<ProgramDTO> getPublicProgramData(
-      @RequestParam(required = true) String name) {
-
-    ProgramEntity programEntity = programService.getProgram(name, true);
-
-    ProgramDTO programDTO = programDTOBuilder.convertEntityToDTO(programEntity);
-
-    return ResponseEntity.ok(programDTO);
+  public ResponseEntity<Object> getPublicProgramData(@RequestParam(required = true) String name) {
+    try {
+      ProgramEntity programEntity = programService.getProgram(name, true);
+      ProgramDTO programDTO = programDTOBuilder.convertEntityToDTO(programEntity);
+      return new ResponseEntity(programDTO, HttpStatus.OK);
+    } catch (Exception e) {
+      if (e.getMessage().contains("NOT_FOUND")) {
+        return new ResponseEntity(
+            "Program '" + name + "' is not found, please enter a valid Program name",
+            HttpStatus.NOT_FOUND);
+      } else {
+        return new ResponseEntity(
+            "Error occurred while fetching the Program Details", HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
   }
 
   @GetMapping(value = "/programs")
